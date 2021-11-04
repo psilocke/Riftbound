3d5cb78123b489a6e6f4e65f18a66b2a68b4f194
GenotypeNull <rhysculley10@gmail.com>
2021-11-04 17:09:23 -0500

    old files synchronization

--- src/main/java/com/psilocke/riftbound/Riftbound.java
+++ src/main/java/com/psilocke/riftbound/Riftbound.java
@@ -25,7 +25,7 @@ import com.psilocke.riftbound.client.render.ModEntityRendererManager;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("riftbound")
 public class Riftbound{
  public static final String MOD_ID = "riftbound";
 	
